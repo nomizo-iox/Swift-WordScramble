b6cd5a3607db8da6403246d786b28c863c6d30b1
nomizo-iox <nomizo.iox@gmail.com>
2019-12-22 18:07:10 -0600

    finished implementation of the scrable game.

--- WordScrable/ContentView.swift
+++ WordScrable/ContentView.swift
@@ -10,18 +10,133 @@ import SwiftUI
 
 struct ContentView: View {
     
-    var body: some View {
+    /* -------------------------------------------------
+     STATE SECTION
+     ----------------------------------------------------*/
+    @State private var usedWords = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
+    @State private var errorTitle = ""
+    @State private var errorMessage = ""
+    @State private var showingError = false
+    
+    
+    /* -------------------------------------------------
+     FUCTIONS SECTION
+     ----------------------------------------------------*/
+    func addNewWord() {
         
-        if let fileURL = Bundle.main.url(forResource: "Some-file", withExtension: "txt") {
-            // We found the file in our bundle!
+        // lowercase and trim the word, to make sure we do not add duplicate words with case differences
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        // exit if the remianing string is empty
+        guard answer.count > 0 else {
+            return
+        }
+        
+        // extra validation to come
+        usedWords.insert(answer, at: 0)
+        newWord = ""
+        
+        
+        /*
+         VALIDATION
+         */
+        // IS ORIGINAL
+        guard isOriginal(word: answer) else {
+            wordError(title: "Word used already", message: "Be more original")
+            return
+        }
+        
+        // IS POSSIBLE
+        guard isPossible(word: answer) else {
+            wordError(title: "Word not recognized", message: "You can't just make them up, you know!")
+            return
+        }
+        
+        // IS REAL
+        guard isReal(word: answer) else {
+            wordError(title: "Word not possible", message: "This isn't a real word.")
+            return
+        }
+        
+    }
+    
+    func startGame() {
+        // 1. Find the URL for start.txt in our app bundle
+        if let startWordURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
             
-            if let fileContent = try? String(contentsOf: fileURL) {
-                // We loaded the file into a string
+            // 2. Load start.txt into a string
+            if let startWords = try? String(contentsOf: startWordURL) {
+                
+                // 3. Split the string up into an array of strings, splitting on line breaks
+                let allWords = startWords.components(separatedBy: "\n")
+                
+                // 4. Pick one random word, or use "silkwork" as a sensible default
+                rootWord = allWords.randomElement() ?? "silkworm"
+                
+                // If we are here everything has worked, so we can exit
+                return
             }
         }
         
-       return Text("Hello, Sam")
+        // if were we are "here" then there was a problem - trigger a crashand report the error
+        fatalError("Could not load start.txt from bundle")
+    }
+    
+    func isOriginal(word: String) -> Bool {
+        !usedWords.contains(word)
+    }
+    
+    func isPossible(word: String) -> Bool {
+        var tempWord = rootWord
         
+        for letter in word {
+            if let pos = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: pos)
+            } else {
+                return false
+            }
+        }
+        return true
+    }
+    
+    func isReal(word: String) -> Bool {
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        
+        return misspelledRange.location == NSNotFound
+    }
+    
+    func wordError(title: String, message: String) {
+        errorTitle = title
+        errorMessage = message
+        showingError = true
+    }
+    
+    /* -------------------------------------------------
+     BODY SECTION
+     ----------------------------------------------------*/
+    var body: some View {
+        NavigationView {
+            VStack {
+                TextField("Enter your word", text: $newWord, onCommit: addNewWord)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .autocapitalization(.none)
+                    .padding()
+                
+                List (usedWords, id:\.self) {
+                    Image(systemName: "\($0.count).circle")
+                    Text($0)
+                }
+            }
+            .navigationBarTitle(rootWord)
+            .onAppear(perform: startGame)
+            .alert(isPresented: $showingError) {
+                Alert(title: Text(errorTitle), message: Text(errorMessage), dismissButton: .default(Text("OK")))
+            }
+        }
     }
 }
 
